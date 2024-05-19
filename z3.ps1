5e7a44e82785b0ac0b8ff08b6d25718e1aac393a
Nick <snicky5746@gmail.com>
2024-05-19 12:22:59 +0200

    First commit

---
+++ z3.ps1
@@ -0,0 +1,78 @@
+﻿$user = Read-Host "Enter your username"
+$pswd = Read-Host  "Enter your password" -AsSecureString
+$actualUser = "admin"
+$actualPassword = ConvertTo-SecureString -String "password" -AsPlainText -Force
+
+function Compare-SecureStrings
+{
+  <#
+  .SYNOPSIS
+  Safely compares two SecureString objects without decrypting them.
+  .DESCRIPTION
+  Safely compares two SecureString objects without decrypting them.
+  Outputs $true if they are equal, or $false otherwise.
+  .INPUTS
+  None. You can't pipe objects to Compare-SecureStrings.
+  .OUTPUTS
+  System.Boolean is returned.
+  .PARAMETER secureString1
+  Specifies first string for comparison
+  .PARAMETER secureString2
+  Specifies second string for comparison
+  .EXAMPLE
+  PS> Compare-SecureString $entered_password $acutual_password
+  false
+  #>
+
+  param(
+    [Security.SecureString]
+    $secureString1,
+
+    [Security.SecureString]
+    $secureString2
+  )
+
+  try {
+    $bstr1 = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString1)
+    $bstr2 = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString2)
+    $length1 = [Runtime.InteropServices.Marshal]::ReadInt32($bstr1,-4)
+    $length2 = [Runtime.InteropServices.Marshal]::ReadInt32($bstr2,-4)
+    if ( $length1 -ne $length2 ) {
+      return $false
+    }
+    for ( $i = 0; $i -lt $length1; ++$i ) {
+      $b1 = [Runtime.InteropServices.Marshal]::ReadByte($bstr1,$i)
+      $b2 = [Runtime.InteropServices.Marshal]::ReadByte($bstr2,$i)
+      if ( $b1 -ne $b2 ) {
+        return $false
+      }
+    }
+    return $true
+
+  }
+  finally {
+    if ( $bstr1 -ne [IntPtr]::Zero ) {
+      [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr1)
+    }
+    if ( $bstr2 -ne [IntPtr]::Zero ) {
+      [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr2)
+    }
+  }
+}
+
+if ($user -eq $actualUser)
+{
+  if (Compare-SecureStrings $pswd $actualPassword)
+  {
+      Write-Host "Successful login"
+  }
+  else
+  {
+      Write-Host "Incorrect password"
+    }
+}
+
+else
+{
+    Write-Host "Wrong login"
+}
---
+++ z5.ps1
@@ -0,0 +1,62 @@
+﻿<#
+  .SYNOPSIS
+  Displays some info about the device (time, system versino, username, ip)
+
+  .DESCRIPTION
+  Displays some info about the device (time, system versino, username, ip)
+
+  .INPUTS
+  None. You can't pipe objects to Update-Month.ps1.
+
+  .OUTPUTS
+  None. Update-Month.ps1 doesn't generate any output.
+
+  .EXAMPLE
+  PS> .\z5.ps1
+    Current date on lenovo :05/19/2024 12:19:36
+    Current system version on lenovo : 10.0.22631
+    Current  user is: Administrator DefaultAccount Guest snick WDAGUtilityAccount
+    Current ipv4 on lenovo is: 192.168.56.1
+#>
+
+
+function Get-Device-Date
+{
+    param(
+        [string] $device
+    )
+    Write-Host "Current date on $device :$(Get-Date)"
+}
+
+function Get-Device-Version
+{
+    param(
+        [string] $device
+    )
+    $ofInfo = Get-CimInstance Win32_OperatingSystem
+    $osInfo = $ofInfo.Version
+    Write-Host "Current system version on $device : $osInfo"
+}
+
+function Get-Device-User
+{
+    param(
+        [string] $device
+    )
+    Write-Host "Current $deivce user is: $(Get-LocalUser)"
+}
+
+function Get-Device-IP
+{
+    param(
+        [string] $device
+    )
+    $ipconfigOutput = ipconfig
+    $ipAddress = ($ipconfigOutput | Where-Object {$_ -match 'IPv4 Address'}).Split(':')[-1].Trim()
+    Write-Host "Current ipv4 on $device is: $ipaddress"
+}
+
+Get-Device-Date lenovo
+Get-Device-Version lenovo
+Get-Device-User lenovo
+Get-Device-IP lenovo
